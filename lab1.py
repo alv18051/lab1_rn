cff787542f09a8c35332ae1d37fd17e7f37a6617
Javier Alvarez <77762715+alv18051@users.noreply.github.com>
2023-07-17 19:32:44 -0600

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def initialize_parameters(n_x, n_h, n_y):
+    W1 = np.random.randn(n_h, n_x)
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h)
+    b2 = np.zeros((n_y, 1))
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    return parameters
+
+def forward_propagation(X, parameters):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+
+    cache = {"A1": A1, "A2": A2}
+    return A2, cache
+
+def compute_cost(A2, Y):
+    m = Y.shape[1]
+    cost = -1/m * np.sum(Y * np.log(A2) + (1 - Y) * np.log(1 - A2))
+    return cost
+
+def backward_propagation(parameters, cache, X, Y):
+    m = X.shape[1]
+
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+
+    W2 = parameters["W2"]
+
+    dZ2 = A2 - Y
+    dW2 = 1/m * np.dot(dZ2, A1.T)
+    db2 = 1/m * np.sum(dZ2, axis=1, keepdims=True)
+
+    dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))
+    dW1 = 1/m * np.dot(dZ1, X.T)
+    db1 = 1/m * np.sum(dZ1, axis=1, keepdims=True)
+
+    grads = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
+    return grads
+
+def update_parameters(parameters, grads, learning_rate=1.2):
+    W1, b1, W2, b2 = parameters["W1"], parameters["b1"], parameters["W2"], parameters["b2"]
+    dW1, db1, dW2, db2 = grads["dW1"], grads["db1"], grads["dW2"], grads["db2"]
+
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+
+    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+    return parameters
+
+def model(X, Y, n_h, num_iterations, learning_rate=1.2, print_cost=False):
+    np.random.seed(3)
+    n_x = X.shape[0]
+    n_y = Y.shape[0]
+
+    parameters = initialize_parameters(n_x, n_h, n_y)
+
+    for i in range(num_iterations):
+        A2, cache = forward_propagation(X, parameters)
+
+        cost = compute_cost(A2, Y)
+
+        grads = backward_propagation(parameters, cache, X, Y)
+
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if print_cost and i % 1000 == 0:
+            print("Cost after iteration {}: {}".format(i, cost))
+
+    return parameters
+
+def predict(X, parameters):
+    A2, cache = forward_propagation(X, parameters)
+    predictions = np.round(A2)
+    return predictions
+
+
+np.random.seed(2)
+x = np.array([[0,0,1,1], [0,1,0,1]])
+print(forward_propagation(x, initialize_parameters(2, 2, 1)))
