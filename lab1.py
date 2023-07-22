7a7de5e39f4f9aefdaddae9af385d28753b72d61
Javier Alvarez <77762715+alv18051@users.noreply.github.com>
2023-07-21 18:31:06 -0600

    yes

--- lab1.py
+++ lab1.py
@@ -1,17 +1,25 @@
 import numpy as np
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def sigmoid(z):
+    return 1/(1 + np.exp(-z))
 
 def initialize_parameters(n_x, n_h, n_y):
     W1 = np.random.randn(n_h, n_x)
+    #b1 = np.random.randn(n_h, 1)
     b1 = np.zeros((n_h, 1))
     W2 = np.random.randn(n_y, n_h)
+    b2 = np.random.randn(n_y, 1)
     b2 = np.zeros((n_y, 1))
-    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
+
+    parameters = {
+        "W1": W1,
+        "b1" : b1,
+        "W2": W2,
+        "b2" : b2
+    }
     return parameters
 
-def forward_propagation(X, parameters):
+def forward_prop(X, parameters):
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
@@ -22,70 +30,95 @@ def forward_propagation(X, parameters):
     Z2 = np.dot(W2, A1) + b2
     A2 = sigmoid(Z2)
 
-    cache = {"A1": A1, "A2": A2}
+    cache = {
+        "A1": A1,
+        "A2": A2
+    }
     return A2, cache
+
+def calculate_cost(A2, Y):
+    cost = -np.sum(np.multiply(Y, np.log(A2)) +  np.multiply(1-Y, np.log(1-A2)))/m
+    cost = np.squeeze(cost)
+
+    return cost
 
 def compute_cost(A2, Y):
     m = Y.shape[1]
     cost = -1/m * np.sum(Y * np.log(A2) + (1 - Y) * np.log(1 - A2))
     return cost
 
-def backward_propagation(parameters, cache, X, Y):
+def backward_prop(X, Y, cache, parameters):
     A1 = cache["A1"]
     A2 = cache["A2"]
 
     W2 = parameters["W2"]
+
     dZ2 = A2 - Y
     dW2 = np.dot(dZ2, A1.T)/m
     db2 = np.sum(dZ2, axis=1, keepdims=True)/m
-    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), 1-np.power(A1, 2))
     dW1 = np.dot(dZ1, X.T)/m
     db1 = np.sum(dZ1, axis=1, keepdims=True)/m
 
-    grads = {"dW1": dW1, "db1": db1, "dW2": dW2, "db2": db2}
+    grads = {
+        "dW1": dW1,
+        "db1": db1,
+        "dW2": dW2,
+        "db2": db2
+    }
+
     return grads
 
-def update_parameters(parameters, grads, learning_rate=1.2):
-    W1, b1, W2, b2 = parameters["W1"], parameters["b1"], parameters["W2"], parameters["b2"]
-    dW1, db1, dW2, db2 = grads["dW1"], grads["db1"], grads["dW2"], grads["db2"]
+def update_parameters(parameters, grads, learning_rate):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
 
-    W1 -= learning_rate * dW1
-    b1 -= learning_rate * db1
-    W2 -= learning_rate * dW2
-    b2 -= learning_rate * db2
+    dW1 = grads["dW1"]
+    db1 = grads["db1"]
+    dW2 = grads["dW2"]
+    db2 = grads["db2"]
 
-    parameters = {"W1": W1, "b1": b1, "W2": W2, "b2": b2}
-    return parameters
+    W1 = W1 - learning_rate*dW1
+    b1 = b1 - learning_rate*db1
+    W2 = W2 - learning_rate*dW2
+    b2 = b2 - learning_rate*db2
+    
+    new_parameters = {
+        "W1": W1,
+        "W2": W2,
+        "b1" : b1,
+        "b2" : b2
+    }
 
-def model(X, Y, n_h, num_iterations, learning_rate=1.2, print_cost=False):
-    np.random.seed(3)
-    n_x = X.shape[0]
-    n_y = Y.shape[0]
-
+    return new_parameters
+def model(X, Y, n_x, n_h, n_y, num_of_iters, learning_rate):
     parameters = initialize_parameters(n_x, n_h, n_y)
 
-    for i in range(num_iterations):
-        A2, cache = forward_propagation(X, parameters)
+    for i in range(0, num_of_iters+1):
+        a2, cache = forward_prop(X, parameters)
 
-        cost = compute_cost(A2, Y)
+        cost = calculate_cost(a2, Y)
 
-        grads = backward_propagation(parameters, cache, X, Y)
+        grads = backward_prop(X, Y, cache, parameters)
 
         parameters = update_parameters(parameters, grads, learning_rate)
 
-        if print_cost and i % 1000 == 0:
-            print("Cost after iteration {}: {}".format(i, cost))
+        if(i%100 == 0):
+            print('Cost after iteration# {:d}: {:f}'.format(i, cost))
 
     return parameters
 
 def predict(X, parameters):
-    A2, cache = forward_propagation(X, parameters)
-    yhat = A2
+    a2, cache = forward_prop(X, parameters)
+    yhat = a2
     yhat = np.squeeze(yhat)
-    if yhat >= 0.5:
+    if(yhat >= 0.5):
         y_predict = 1
     else:
         y_predict = 0
+
     return y_predict
     
 
