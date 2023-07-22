e49adf9927bc24e084e915db1413cbe437b08693
Javier Alvarez <77762715+alv18051@users.noreply.github.com>
2023-07-21 18:32:16 -0600

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -124,5 +124,32 @@ def predict(X, parameters):
 
 
 np.random.seed(2)
-x = np.array([[0,0,1,1], [0,1,0,1]])
-print(forward_propagation(x, initialize_parameters(2, 2, 1)))
+
+# The 4 training examples by columns
+X = np.array([[0, 0, 1, 1], [0, 1, 0, 1]])
+
+# The outputs of the XOR for every example in X
+Y = np.array([[0, 1, 1, 0]])
+
+# No. of training examples
+m = X.shape[1]
+
+# Set the hyperparameters
+n_x = 2     #No. of neurons in first layer
+n_h = 2     #No. of neurons in hidden layer
+n_y = 1     #No. of neurons in output layer
+num_of_iters = 1000
+learning_rate = 0.3
+
+trained_parameters = model(X, Y, n_x, n_h, n_y, num_of_iters, learning_rate)
+
+# Test 2X1 vector to calculate the XOR of its elements. 
+# Try (0, 0), (0, 1), (1, 0), (1, 1)
+X_test = np.array([[1], [1]])
+
+print(trained_parameters)
+
+y_predict = predict(X_test, trained_parameters)
+
+print('Neural Network prediction for example ({:d}, {:d}) is {:d}'.format(
+    X_test[0][0], X_test[1][0], y_predict))
